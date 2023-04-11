29fbf335d91509b8742fa71700e3ca64ea224e71
Fredde <welandfr@arcada.fi>
2023-04-11 13:50:12 +0300

    lektion 3 mellancommit

--- app/src/main/java/fi/arcada/sos_23_exempel/Statistics.java
+++ app/src/main/java/fi/arcada/sos_23_exempel/Statistics.java
@@ -1,18 +1,36 @@
 package fi.arcada.sos_23_exempel;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Statistics {
 
     public static String greet() {
         return "Hej";
     }
 
-    public static double calcMean(double[] values) {
+    // Metod för att sortera data
+    public static ArrayList<Double> sortValues(ArrayList<Double> values) {
+        // vi skapar en kopia så vi inte sorterar ursprungliga datamängden
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        Collections.sort(sorted);
+        return sorted;
+    }
+
+    public static double calcMedian(ArrayList<Double> values) {
+        ArrayList<Double> sorted = sortValues(values);
+        int middle = sorted.size()/2;
+        return sorted.get(middle);
+    }
+
+    public static double calcMean(ArrayList<Double> values) {
         double total = 0;
-        for (int i=0; i < values.length; i++) {
-            total += values[i];
+        for (int i = 0; i < values.size(); i++) {
+            total += values.get(i);
         }
 
-        return total / values.length;
+        return total / values.size();
 
     }
 
