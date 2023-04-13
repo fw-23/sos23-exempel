0d0e366b047676b52eda42709fc96230df0a0a46
Fredde <welandfr@arcada.fi>
2023-04-13 13:55:43 +0300

    lektion 4 mellancommit

--- app/src/main/java/fi/arcada/sos_23_exempel/DataItem.java
+++ app/src/main/java/fi/arcada/sos_23_exempel/DataItem.java
@@ -3,13 +3,18 @@ package fi.arcada.sos_23_exempel;
 public class DataItem {
 
     private String name;
+    private double value;
 
-    public DataItem(String name) {
+    public DataItem(String name, double value) {
         this.name = name;
+        this.value = value;
     }
 
     public String getName() {
         return name;
+    }
+    public double getValue() {
+        return value;
     }
 
 }
