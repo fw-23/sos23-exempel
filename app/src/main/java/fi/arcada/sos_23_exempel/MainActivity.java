cf9b8d195da49f0d571df83c539a73a560b88607
Fredde <welandfr@arcada.fi>
2023-04-18 14:56:24 +0300

    lektion 5

--- app/src/main/java/fi/arcada/sos_23_exempel/MainActivity.java
+++ app/src/main/java/fi/arcada/sos_23_exempel/MainActivity.java
@@ -10,14 +10,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     // deklarera variabler
     TextView outputText;
-    EditText inputText;
+    EditText inputText, inputValue;
     RecyclerView recyclerView;
+    CustomAdapter adapter;
 
     ArrayList<DataItem> dataItems = new ArrayList<>();
     ArrayList<Double> values = new ArrayList<>();
@@ -27,56 +27,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dataItems = Statistics.getSampleData();
-
-        // for-loop
-        /*for (int i = 0; i < dataItems.size(); i++) {
-            values.add(dataItems.get(i).getValue());
-        }*/
-        // samma sak med en for each-loop
-        for (DataItem item: dataItems) {
-            values.add(item.getValue());
-        }
+        //dataItems = Statistics.getSampleData();
 
         // Vi skapar en koppling mellan vår Java-kod och vår XML-layout
         outputText = findViewById(R.id.outputText);
-        inputText = findViewById(R.id.editTextName);
+        inputText = findViewById(R.id.dataName);
+        inputValue = findViewById(R.id.dataValue);
         recyclerView = findViewById(R.id.dataItemsRecyclerView);
 
-        CustomAdapter adapter = new CustomAdapter(this, dataItems);
+        adapter = new CustomAdapter(this, dataItems);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-        DataItem mittObjekt = new DataItem("Helsingfors", 658457);
-        DataItem mittAndraObjekt = new DataItem("Esbo", 297132);
-
-        // HashMap-exempel
-        HashMap<String,Double> cities = new HashMap<>();
-        cities.put("Helsingfors", 658457.0);
-        cities.put("Esbo", 297132.0);
-
-        HashMap<String,DataItem> cityItems = new HashMap<>();
-        cityItems.put("manse", new DataItem("Tammerfors", 244223));
-        /* JavaScript: cityItems = { manse: { name: "Tammerfors", value: 244223 }}
-        *   cityItems.manse.name
-        * */
-
-        outputText.setText(String.format("%s: %.2f\nEsbo: %.2f\n%s: %.2f",
-                mittObjekt.getName(),
-                mittObjekt.getValue(),
-                cities.get("Esbo"),
-                cityItems.get("manse").getName(),
-                cityItems.get("manse").getValue()
-        ));
-
-
     }
 
     public void calculate(View view) {
 
-        outputText.setText(String.format("Hej %s\n\nMedelvärde: %.2f\nMedian: %.2f\nStandardavvikelse: %.2f",
-                inputText.getText(),
+        dataItems.add(new DataItem(
+                inputText.getText().toString(),
+                Double.parseDouble(inputValue.getText().toString())
+        ));
+
+        adapter.notifyDataSetChanged();
+
+        // Skapa values-arraylist med endast värden
+        for (DataItem item: dataItems) {
+            values.add(item.getValue());
+        }
+
+        if (values.size() < 2) {
+            outputText.setText("Mera data behövs...");
+            return;
+        }
+
+        outputText.setText(String.format("Medelvärde: %.2f\nMedian: %.2f\nStandardavvikelse: %.2f",
                 Statistics.calcMean(values),
                 Statistics.calcMedian(values),
                 Statistics.calcSD(values)
